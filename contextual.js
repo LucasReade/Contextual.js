744d9f4fbb185401d0e1c503960ed0d3adcb5fc7
Lucas Reade <lucas.reade@yahoo.com>
2020-03-04 09:05:25 +0000

    minor fixes

--- contextual.js
+++ contextual.js
@@ -33,7 +33,7 @@ class Contextual{
      * @param {ContextualItem} item item to add to the contextual menu
      */
     add(item){
-        this.menuControl.appendChild(item);
+        this.menuControl.appendChild(item.element);
     }
     /**
      * Makes this contextual menu visible
@@ -77,7 +77,7 @@ class ContextualItem{
      * @param {string} [opts.shortcut]
      * @param {void} [opts.onClick]
      * @param {boolean} [opts.enabled]
-     * @Param {Array<ContextualItem>} [opts.items]
+     * @param {Array<ContextualItem>} [opts.items]
      * 
      */
     constructor(opts){
